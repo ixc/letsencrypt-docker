aeca45171d36f8b5a8f99f0c7ae2f1c3dc600ce1
Tai Lee <real.human@mrmachine.net>
2017-04-18 17:14:51 +1000

    Always combine key and cert for HAproxy and reload.
    Nothing more annoying than generating a cert that isn't applied (for some reason/error) then having to manually fix it because the script no longer thinks anything has been updated.

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -10,25 +10,24 @@ CERTS=(${DOMAINS//;/ })
 
 # Create or renew certificates.
 for DOMAINS in "${CERTS[@]}"; do
-	if certbot certonly \
-			--agree-tos \
-			--domains "$DOMAINS" \
-			--email "$EMAIL" \
-			--expand \
-			--noninteractive \
-			--webroot \
-			--webroot-path /opt/www \
-			$OPTIONS; then
-		UPDATED=1
-	fi
+	certbot certonly \
+		--agree-tos \
+		--domains "$DOMAINS" \
+		--email "$EMAIL" \
+		--expand \
+		--noninteractive \
+		--webroot \
+		--webroot-path /opt/www \
+		$OPTIONS
 done
 
-# Combine private key and full certificate chain for HAproxy and restart.
-if [[ -n "${UPDATED+1}" && -n "${HAPROXY_IMAGE+1}" ]]; then
-	cd /etc/letsencrypt/live
-	for domain in *; do
-		cat "$domain/privkey.pem" "$domain/fullchain.pem" > "/certs/$domain.pem"
-	done
+# Combine private key and full certificate chain for HAproxy.
+cd /etc/letsencrypt/live
+for domain in *; do
+	cat "$domain/privkey.pem" "$domain/fullchain.pem" > "/certs/$domain.pem"
+done
+# Reload HAproxy.
+if [[ -n "${HAPROXY_IMAGE+1}" ]]; then
 	for container in $(docker ps -f ancestor="$HAPROXY_IMAGE" -f status=running -f volume=/etc/letsencrypt -q); do
 		docker exec "$container" /reload.sh
 	done
