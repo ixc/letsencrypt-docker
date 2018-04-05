8252ed874ad0e31bb5860dd30ecddea6967fc567
Tai Lee <real.human@mrmachine.net>
2018-04-05 15:42:57 +1000

    Do not require `DOMAINS` environment variable.
    We want to deploy a Let's Encrypt service to new swarm clusters, before we have any DNS configured.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Check for required environment variables.
-for var in DOMAINS EMAIL; do
+for var in EMAIL; do
     eval [[ -z \${$var+1} ]] && {
         >&2 echo "ERROR: Missing required environment variable: $var"
         exit 1
